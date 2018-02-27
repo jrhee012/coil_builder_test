eb0a677b3bac436c9737bf151a78a51f6e6c1a94
Jaehyuk Rhee <jaehyuk.rhee@gmail.com>
2018-02-27 13:53:36 -0500

    Initial Commit

---
+++ CoilBuilderTest/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  CoilBuilderTest
+//
+//  Created by Jaehyuk Rhee on 2/27/18.
+//  Copyright © 2018 Jaehyuk Rhee. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
